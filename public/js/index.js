4bdc0ab8ceafc23af9d6b19423f5131c70782ad5
Muhamma Mubashir Nazir <mubashirkhannazir1@google.com>
2019-04-06 17:21:39 -0700

    Add css for Chat page

--- public/js/index.js
+++ public/js/index.js
@@ -15,19 +15,46 @@ socket.on('newMessage', function (message) {
     jQuery('#messages').append(li);
 });
 
-// socket.emit('createMessage', {
-//     from:'Mubashir',
-//     text: 'trying to get a callback'
-// }, function() {
-//     console.log('okay got it!');
-// });
+socket.on('newLocationMessage', function(message) {
+     var li = jQuery('<li></li>');
+     var a = jQuery('<a target="_blank">my current location</a>');
+     
+     li.text(`${message.from}`);
+     a.attr('href', message.url);
+     li.append(a);
+     jQuery('#messages').append(li);
+});
 
 jQuery('#message-form').on('submit', function(e) {
+    var messageTextBox = jQuery('[name="message"]');
+
     e.preventDefault();
+    
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name="message"]').val()
+        text: messageTextBox.val()
     }, function() {
-
+        messageTextBox.val('');
     });
 });
+
+var locationButton = jQuery('#sendLocation');
+
+locationButton.on('click', function() {
+    if(!navigator.geolocation){
+        return alert('Geolocation not supported');
+    }
+
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
+    navigator.geolocation.getCurrentPosition(function(position){
+        locationButton.removeAttr('disabled').text('Send location');
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude, 
+            longitude: position.coords.longitude
+        });
+    }, function(e){
+        locationButton.removeAttr('disabled').text('Send location');
+        alert('unable to fetch location');
+    });
+}); 
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -23,20 +23,12 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message,callback) => {
         console.log('new Message', message);
-        //setTimeout(3000);
-        
-        
-io.emit('newMessage',generateMessage(message.from, message.text)); 
-        //{
-        //     from: message.from,
-        //     text:message.text,
-        //     createdAt: new Date().getTime()
-        // });
-        //socket.broadcast.emit('newMessage', {
-        //     from: message.from,
-        //     text:message.text,
-        //     createdAt: new Date().getTime()
-        // });
+        io.emit('newMessage',generateMessage(message.from, message.text)); 
+        callback( );
+    });
+
+    socket.on('createLocationMessage', (coords, message) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
 
     socket.on('disconnect', () => {
