931ecd0a781fc31a774bb40ad81d12db908d4505
Muhamma Mubashir Nazir <mubashirkhannazir1@google.com>
2019-04-08 05:14:14 -0700

    Add Formatted Timestamp using moment

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+//const moment = require('moment');
+
 var socket = io();
 socket.on('connect', function() {
     console.log('connection established');
@@ -9,20 +11,23 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function (message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     console.log('new message from server', message);
     var li= jQuery('<li></li>');
-    li.text(`${message.from}:${message.text}`);
+    li.text(`${message.from}, ${formattedTime}: ${message.text}`);
     jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message) {
-     var li = jQuery('<li></li>');
-     var a = jQuery('<a target="_blank">my current location</a>');
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">my current location</a>');
      
-     li.text(`${message.from}`);
-     a.attr('href', message.url);
-     li.append(a);
-     jQuery('#messages').append(li);
+    li.text(`${message.from}, ${formattedTime}: `);
+    a.attr('href', message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function(e) {
